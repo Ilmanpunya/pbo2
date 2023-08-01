6056db9a497c2f6680e174d75f81d1cb2ee201c6
Ilmanpunya <127816851+Ilmanpunya@users.noreply.github.com>
2023-08-01 11:56:23 +0700

    Add files via upload

---
+++ tugas besar/Komputer.py
@@ -0,0 +1,87 @@
+import requests
+import json
+class Komputer:
+    def __init__(self):
+        self.__id=None
+        self.__id_komputer = None
+        self.__Billing = None
+        self.__Jenis = None
+        self.__Status = None
+        self.__url = "http://f0833063.xsph.ru/appwarnet/komputer_api.php"
+                    
+    @property
+    def id(self):
+        return self.__id
+    @property
+    def id_komputer(self):
+        return self.__id_komputer
+        
+    @id_komputer.setter
+    def id_komputer(self, value):
+        self.__id_komputer = value
+    @property
+    def Billing(self):
+        return self.__Billing
+        
+    @Billing.setter
+    def Billing(self, value):
+        self.__Billing = value
+    @property
+    def Jenis(self):
+        return self.__Jenis
+        
+    @Jenis.setter
+    def Jenis(self, value):
+        self.__Jenis = value
+    @property
+    def Status(self):
+        return self.__Status
+        
+    @Status.setter
+    def Status(self, value):
+        self.__Status = value
+    def get_all(self):
+        payload ={}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.__url, json=payload, headers=headers)
+        return response.text
+    def get_by_id_komputer(self, id_komputer):
+        url = self.__url+"?id_komputer="+id_komputer
+        payload = {}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(url, json=payload, headers=headers)
+        data = json.loads(response.text)
+        for item in data:
+            self.__id = item['ID']
+            self.__id_komputer = item['id_komputer']
+            self.__Billing = item['Billing']
+            self.__Jenis = item['Jenis']
+            self.__Status = item['Status']
+        return data
+    def simpan(self):
+        payload = {
+            "id_komputer":self.__id_komputer,
+            "Billing":self.__Billing,
+            "Jenis":self.__Jenis,
+            "Status":self.__Status
+            }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.post(self.__url, data=payload, headers=headers)
+        return response.text
+    def update_by_id_komputer(self, id_komputer):
+        url = self.__url+"?id_komputer="+id_komputer
+        payload = {
+            "id_komputer":self.__id_komputer,
+            "Billing":self.__Billing,
+            "Jenis":self.__Jenis,
+            "Status":self.__Status
+            }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.put(url, data=payload, headers=headers)
+        return response.text
+    def delete_by_id_komputer(self,id_komputer):
+        url = self.__url+"?id_komputer="+id_komputer
+        headers = {'Content-Type': 'application/json'}
+        payload={}
+        response = requests.delete(url, json=payload, headers=headers)
+        return response.text
---
+++ tugas besar/Transaksi.py
@@ -0,0 +1,76 @@
+import requests
+import json
+class Transaksi:
+    def __init__(self):
+        self.__id=None
+        self.__id_pelanggan = None
+        self.__Billing = None
+        self.__waktu = None
+        self.__url = "http://f0833063.xsph.ru/appwarnet/transaksi_api.php"
+                    
+    @property
+    def id(self):
+        return self.__id
+    @property
+    def id_pelanggan(self):
+        return self.__id_pelanggan
+        
+    @id_pelanggan.setter
+    def id_pelanggan(self, value):
+        self.__id_pelanggan = value
+    @property
+    def Billing(self):
+        return self.__Billing
+        
+    @Billing.setter
+    def Billing(self, value):
+        self.__Billing = value
+    @property
+    def waktu(self):
+        return self.__waktu
+        
+    @waktu.setter
+    def waktu(self, value):
+        self.__waktu = value
+    def get_all(self):
+        payload ={}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(self.__url, json=payload, headers=headers)
+        return response.text
+    def get_by_id_pelanggan(self, id_pelanggan):
+        url = self.__url+"?id_pelanggan="+id_pelanggan
+        payload = {}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.get(url, json=payload, headers=headers)
+        data = json.loads(response.text)
+        for item in data:
+            self.__id = item['id_transaksi']
+            self.__id_pelanggan = item['id_pelanggan']
+            self.__Billing = item['Billing']
+            self.__waktu = item['waktu']
+        return data
+    def simpan(self):
+        payload = {
+            "id_pelanggan":self.__id_pelanggan,
+            "Billing":self.__Billing,
+            "waktu":self.__waktu
+            }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.post(self.__url, data=payload, headers=headers)
+        return response.text
+    def update_by_id_pelanggan(self, id_pelanggan):
+        url = self.__url+"?id_pelanggan="+id_pelanggan
+        payload = {
+            "id_pelanggan":self.__id_pelanggan,
+            "Billing":self.__Billing,
+            "waktu":self.__waktu
+            }
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.put(url, data=payload, headers=headers)
+        return response.text
+    def delete_by_id_pelanggan(self,id_pelanggan):
+        url = self.__url+"?id_pelanggan="+id_pelanggan
+        headers = {'Content-Type': 'application/json'}
+        payload={}
+        response = requests.delete(url, json=payload, headers=headers)
+        return response.text
